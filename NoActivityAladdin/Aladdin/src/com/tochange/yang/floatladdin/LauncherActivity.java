28271eee4751a3992ac43dd16c3ed78143facc47
tochange <tochange2011@gmail.com>
2014-07-27 22:03:11 +0800

    formatted a file

--- NoActivityAladdin/Aladdin/src/com/tochange/yang/floatladdin/LauncherActivity.java
+++ NoActivityAladdin/Aladdin/src/com/tochange/yang/floatladdin/LauncherActivity.java
@@ -9,22 +9,22 @@ import com.tochange.yang.lib.Utils;
 
 public class LauncherActivity extends Activity {
     
-    public static final boolean DEBUG_MODE                      = false;
-    public static final String KEY_X                            = "x";
-    public static final String KEY_Y                            = "y";
-    public static final String KEY_IMAGE                        = "image";
-    public static final String DELETED_BUTTON_TEXT_CONTENT      = " × ";
-    public static final int DELETED_BUTTON_TEXT_SIZE            = 25 ;
-    public static final int VERTICAL_OFFSET                     = 15;
-    public static final int ANIMATION_TIME_SHORT                = 600;//600 
-    public static final int ANIMATION_TIME_LONG                 = 2000; 
-    public static final int ANIMATION_SPLIT_HORIZONTAL          = 40; 
-    public static final int ANIMATION_SPLIT_VERTICAL            = 40; 
+    public static final boolean   DEBUG_MODE                                = false;
+    public static final String    KEY_X                                     = "x";
+    public static final String    KEY_Y                                     = "y";
+    public static final String    KEY_IMAGE                                 = "image";
+    public static final String    DELETED_BUTTON_TEXT_CONTENT               = " × ";
+    public static final int       DELETED_BUTTON_TEXT_SIZE                  = 25 ;
+    public static final int       VERTICAL_OFFSET                           = 15;
+    public static final int       ANIMATION_TIME_SHORT                      = 600;//600 
+    public static final int       ANIMATION_TIME_LONG                       = 2000; 
+    public static final int       ANIMATION_SPLIT_HORIZONTAL                = 40; 
+    public static final int       ANIMATION_SPLIT_VERTICAL                  = 40; 
     
-    public static boolean IS_TABLTE;        
-    public static int ANIMATION_TIME;      
-    public static int SCREEN_WIDTH;     
-    public static int SCREEN_HEIGHT;     
+    public static boolean         IS_TABLTE;        
+    public static int             ANIMATION_TIME;      
+    public static int             SCREEN_WIDTH;     
+    public static int             SCREEN_HEIGHT;     
     
 	@Override 
 	protected void onCreate(Bundle savedInstanceState) {
